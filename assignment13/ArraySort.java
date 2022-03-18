d463778a752239bcfcedda0534b46c15ee2caa36
bathrinath2021 <82258050+bathrinath2021@users.noreply.github.com>
2022-03-18 23:21:32 +0530

    Add files via upload
    Uploaded succesfully

---
+++ assignment13/ArraySort.java
@@ -0,0 +1,13 @@
+package assignment13;
+
+import java.util.Arrays;
+
+public class ArraySort {
+
+	public static void main(String[] args) {
+		String[] s = { "V", "A", "Z", "P" };
+		int i = Arrays.binarySearch(s, "Z");
+		System.out.println(i);
+	}
+
+}
